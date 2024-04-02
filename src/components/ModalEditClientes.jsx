8d19d68f272fa906e8951f4bdda83418773b93c7
kfvillalba <kfvillalba@unicesar.edu.co>
2024-04-01 21:18:51 -0500

    Validaciones

--- src/components/ModalEditClientes.jsx
+++ src/components/ModalEditClientes.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import Swal from 'sweetalert2'
+import React from "react";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 const ModalEditClientes = ({ open, onClose, editar, dataCliente }) => {
   const {
@@ -8,103 +8,114 @@ const ModalEditClientes = ({ open, onClose, editar, dataCliente }) => {
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm()
+  } = useForm();
   const onSubmit = async (data) => {
     try {
       const response = await fetch(
         `https://localhost:7127/api/Clientes/Actualizar?id=${dataCliente.id}`,
         {
-          method: 'PUT',
+          method: "PUT",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(data),
         }
-      )
+      );
 
       if (response.ok) {
-        editar(data)
-        onClose()
-        reset()
+        editar(data);
+        onClose();
+        reset();
         Swal.fire({
-          icon: 'success',
-          title: 'Cliente editado con exito',
+          icon: "success",
+          title: "Cliente editado con exito",
           showConfirmButton: false,
           timer: 1500,
-        })
+        });
       } else {
-        throw new Error('Error al guardar al cliente')
+        throw new Error("Error al guardar al cliente");
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
       Swal.fire({
-        icon: 'error',
-        title: 'Error al guardar al cliente',
+        icon: "error",
+        title: "Error al guardar al cliente",
         text: error.message,
-      })
+      });
     }
-  }
-  if (!open) return null
+  };
+  if (!open) return null;
   return (
-    <div className='fixed w-full top-0 left-0 h-full z-10 flex items-center justify-center bg-black/50'>
-      <div className=''>
+    <div className="fixed w-full top-0 left-0 h-full z-10 flex items-center justify-center bg-black/50">
+      <div className="">
         <form
-          className='bg-white rounded-lg shadow-sm p-5'
+          className="bg-white rounded-lg shadow-sm p-5"
           onSubmit={handleSubmit(onSubmit)}
         >
           <div>
-            <label htmlFor='nombre' className='label__form'>
+            <label htmlFor="nombre" className="label__form">
               Nombre del cliente
             </label>
             <input
               defaultValue={`${dataCliente.nombre}`}
-              id='nombre'
-              type='text'
-              className='input__form'
-              {...register('nombre', {
+              id="nombre"
+              type="text"
+              className="input__form"
+              {...register("nombre", {
                 required: {
                   value: true,
-                  message: 'El nombre es obligatorio',
+                  message: "El nombre es obligatorio",
                 },
               })}
             />
-            <span className='message'>{errors?.nombre?.message}</span>
+            <span className="message">{errors?.nombre?.message}</span>
           </div>
           <div>
-            <label htmlFor='celular' className='label__form'>
+            <label htmlFor="celular" className="label__form">
               Telefono del cliente
             </label>
             <input
-              defaultValue={`${dataCliente.celular}`}
-              id='celular'
-              type='text'
-              className='input__form'
-              {...register('celular', {
+              id="celular"
+              type="text"
+              className="input__form"
+              {...register("celular", {
                 required: {
                   value: true,
-                  message: 'El telefono es obligatorio',
+                  message: "El telefono es obligatorio",
+                },
+                minLength: {
+                  value: 10,
+                  message: "El numero debe tener 10 Numeros",
+                },
+                maxLength: {
+                  value: 10,
+                  message: "El numero debe tener 10 Numeros",
                 },
               })}
             />
-            <span className='message'>{errors?.telefono?.message}</span>
+            <span className="message">{errors?.telefono?.message}</span>
           </div>
           <div>
-            <label htmlFor='correo' className='label__form'>
+            <label htmlFor="correo" className="label__form">
               Correo del cliente
             </label>
             <input
-              defaultValue={`${dataCliente.correo}`}
-              id='correo'
-              type='text'
-              className='input__form'
-              {...register('correo', {
+              id="correo"
+              type="email"
+              placeholder="Correo electrónico"
+              className="bg-gray-200 border rounded text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2"
+              {...register("correo", {
                 required: {
                   value: true,
-                  message: 'El correo es obligatorio',
+                  message: "El correo es obligatorio",
+                },
+                pattern: {
+                  value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/,
+                  message: "El correo no es valido",
                 },
               })}
             />
-            <span className='message'>{errors?.correo?.message}</span>
+            <span className="message">{errors?.correo?.message}</span>
           </div>
           {/* <div>
             <label htmlFor='direccion' className='label__form'>
@@ -123,15 +134,15 @@ const ModalEditClientes = ({ open, onClose, editar, dataCliente }) => {
             />
             <span className='message'>{errors?.direccion?.message}</span>
           </div> */}
-          <div className='flex gap-4 justify-center'>
-            <button type='submit' className='bnt__primary mt-3'>
+          <div className="flex gap-4 justify-center">
+            <button type="submit" className="bnt__primary mt-3">
               Aceptar
             </button>
             <button
               onClick={() => {
-                reset(), onClose()
+                reset(), onClose();
               }}
-              className='bnt__danger mt-3 '
+              className="bnt__danger mt-3 "
             >
               Cancelar
             </button>
@@ -139,7 +150,7 @@ const ModalEditClientes = ({ open, onClose, editar, dataCliente }) => {
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ModalEditClientes
+export default ModalEditClientes;
